0f5bbc18515d428278cc7afd44d4fdc7979d86e0
Ehren Castillo <ehrencastillo@gmail.com>
2025-04-14 13:02:35 +0800

    feat(models): add camera model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Camera(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.TextField()
+
+    def __str__(self):
+        return f'Camera "{self.name}" | Location: {self.location}'
