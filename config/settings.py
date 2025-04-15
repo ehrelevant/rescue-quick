21537c52e69d53499022d7739d335a2ac4789026
Ehren Castillo <ehrencastillo@gmail.com>
2025-04-15 17:37:00 +0800

    feat(config/settings): move other security risks to env

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ct1g0j6=7b%a7o!oxqr@p2@)k2x+^shp*-=v0z!!dqt*i-%a$l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', False))
 
 ALLOWED_HOSTS = []
 
