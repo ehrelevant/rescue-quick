b5bf4c4826954d367d26435563985f669fdb9d8c
pingu204 <73891290+pingu204@users.noreply.github.com>
2025-04-24 14:36:21 +0800

    feat(ui): convert monitor cards to components

--- core/views.py
+++ core/views.py
@@ -5,6 +5,19 @@ from .models import Camera
 
 def index(request):
     camera_list = Camera.objects.all()
-    context = {'camera_list': camera_list}
+    context = {
+        'monitors' : [
+            {
+                'location' : 'Alumni Engineers Centennial Hall, 4F',
+                'camera_name' : 'ESP3123',
+                'date' : 'April 20, 2025',
+                'num_people' : 2,
+                'num_pets' : 3,
+                'flood_level' : 0.65,
+                'max_flood_level' : 1.75,
+            }
+        ]
+        
+    }
 
     return render(request, 'core/index.html.j2', context)
