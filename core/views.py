0d97c00b827b03b14ac84cc82ef98e905441b84d
pingu204 <73891290+pingu204@users.noreply.github.com>
2025-04-25 22:24:46 +0800

    feat(ui): add panel and components for operations

--- core/views.py
+++ core/views.py
@@ -16,8 +16,26 @@ def index(request):
                 'flood_level' : 0.65,
                 'max_flood_level' : 1.75,
             }
+        ],
+        'operations' : [
+            {
+                'location' : 'Alumni Engineers Centennial Hall, 4F',
+                'camera_name' : 'ESP3123',
+                'date' : 'April 20, 2025',
+                'time_elapsed' : '2 min ago',
+            }
+        ],
+        'done' : [
+            {
+                'location' : 'Alumni Engineers Centennial Hall, 4F',
+                'camera_name' : 'ESP3123',
+                'date' : 'April 20, 2025',
+                'marked_safe' : '11:00 AM',
+                'num_people' : 2,
+                'num_pets' : 3,
+                'flood_level' : 0.65,
+            }
         ]
-        
     }
 
     return render(request, 'core/index.html.j2', context)
