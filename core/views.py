d790ea74735eacb7b74f923efceb69cc044eae35
Airiseru <dmdee2004@gmail.com>
2025-04-28 18:08:38 +0800

    feat(views.py): HTTP GET and POST functionality for ESP32 Cam

--- core/views.py
+++ core/views.py
@@ -1,15 +1,20 @@
+import base64
 from django.shortcuts import render
 
-from .models import SensorCamera, SensorLogs
+from .models import SensorCamera, SensorLogs, CameraLogs
 
+from django.http import HttpRequest
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from uuid import uuid4
 
 import json
 
 
-def index(request):
+def index(request: HttpRequest):
     # Currently just taking the frst entry (i.e. assume only 1 camera)
     # Should eventually become a list of the first entry of each unique pair_id
     # sensor_data = SensorCamera.objects.order_by('-timestamp').first()
@@ -50,7 +55,7 @@ def index(request):
     return render(request, 'core/index.html.j2', context)
 
 
-def feed(request, monitor_id: int):
+def feed(request: HttpRequest, monitor_id: int):
     context = {
         'monitor_id': monitor_id,
         'location': 'Alumni Engineers Centennial Hall, 4F',
@@ -76,7 +81,7 @@ def feed(request, monitor_id: int):
 # Remove csrf_exempt eventually
 @csrf_exempt
 @require_POST
-def post_sensor_data(request):
+def post_sensor_data(request: HttpRequest):
     # print("=== Incoming Request ===")
     # print("Method:", request.method)
     # print("Headers:", dict(request.headers))
@@ -108,5 +113,66 @@ def post_sensor_data(request):
         )
 
         return JsonResponse({'status': 'success'})
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+
+
+@csrf_exempt
+@require_GET
+def get_flood_status(request: HttpRequest):
+    # Get the pair id from the request url
+    pair_id: str | None = request.GET.get('pair_id')
+    sensor_cam: SensorCamera | None = None
+
+    if pair_id is not None and pair_id.isdigit():
+        pair_id_int: int = int(pair_id)
+
+        try:
+            # Find the appropriate Sensor Camera Pair
+            sensor_cam = SensorCamera.objects.get(pair_id=pair_id_int)
+
+            # Get the latest indicator
+            indicator: str = str(sensor_cam.current_depth >= sensor_cam.threshold_depth).lower()
+
+            # Return as a JSON Response
+            return JsonResponse({'status': 'success', 'indicator': indicator})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid pair id'}, status=400)
+
+
+@csrf_exempt
+@require_POST
+def post_image(request: HttpRequest, pair_id: str):
+    try:
+        # Get Sensor Camera Pair ID
+        sensor_cam: SensorCamera | None = None
+        data = json.loads(request.body)
+        img_data = data.get('image')
+        
+        if not img_data:
+            return JsonResponse({'status': 'error', "message": "No image uploaded"}, status=400)
+        
+        # Convert base64 image to actual image
+        decoded_img = base64.b64decode(img_data)
+        img_name = f'{uuid4()}.jpg'
+        img_file = ContentFile(decoded_img, name=img_name)
+
+        # Find appropriate Sensor Camera Pair
+        if pair_id.isdigit():
+            pair_id_int: int = int(pair_id)
+            sensor_cam = SensorCamera.objects.get(pair_id=pair_id_int)
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Invalid camera ID'}, status=400)
+
+        # Add image to camera logs
+        CameraLogs.objects.create(
+            camera_id=sensor_cam,
+            flood_number=sensor_cam.flood_number,
+            image=img_file
+        )
+
+        return JsonResponse({"status": "success", "message": "Upload successful", "filename": img_name})
     except Exception as e:
         return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
