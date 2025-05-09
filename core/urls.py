734e3079e4770e627891f9c94abf7b55b3699a53
Njlr41 <nicholasjohnreyes@gmail.com>
2025-05-09 10:57:54 +0800

    chore: update urls of camera

--- core/urls.py
+++ core/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('feed/<int:pair_id>/', views.feed, name='feed'),
     path('api/sensor-data/<int:pair_id>/', views.post_sensor_data, name='sensor_data'),
     path('api/get-flood-status/', views.get_flood_status, name='get_flood_status'),
-    path('api/upload-image/<str:pair_id>', views.post_image, name='upload_image'),
+    path('api/upload-image/<int:pair_id>', views.post_image, name='upload_image'),
     path('api/pair-id', views.get_available_pair_id, name='available_pair_id'),
     path('api/pair-id/reserve', views.post_reserve_pair_id, name='reserve_pair_id'),
     path(
-        'api/post-cam-health/<str:pair_id>',
+        'api/post-cam-health/<int:pair_id>',
         views.post_cam_health,
         name='post_cam_health',
     ),
