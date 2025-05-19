f9d469428b5cdabccad28cd8526c6fbff75f2b46
Ehren Castillo <ehrencastillo@gmail.com>
2025-05-19 17:40:43 +0800

    feat(admin): register RescuerContacts to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import CameraLogs, SensorLogs, SensorCamera
+from .models import CameraLogs, SensorLogs, SensorCamera, RescuerContacts
 
 admin.site.register(CameraLogs)
 admin.site.register(SensorLogs)
 admin.site.register(SensorCamera)
+admin.site.register(RescuerContacts)
